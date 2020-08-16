353b99c27b3829054375d65b0cdf3b00fdb64602
Vladimir Goncharov <morozovsk@gmail.com>
2020-08-16 18:21:32 +0300

    types

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # opencv-dev is useful for debug
 
-tested on:
-* ubuntu 20.04
-* nodejs v14.6.0
-
 installation:
 * npm install opencv-dev
 
 compiled by emscripten with flags:
 * ASSERTIONS=2
 * DISABLE_EXCEPTION_CATCHING=0
+
+tested on:
+* ubuntu 20.04
+* nodejs v14.6.0
--- index.d.ts
+++ index.d.ts
@@ -1,14 +1,2 @@
-export * from './types/opencv.d';
-import * as OpenCV from './types/opencv.d';
-
-/**
- * OpenCV Object
- */
-export const cv: typeof OpenCV;
-
-/**
- * Translate error number from OpenCV into a meaningful message
- * @param cvObject OpenCV object
- * @param err OpenCV error number 
- */
-export function cvTranslateError(cvObject: typeof OpenCV, err: any): string | Error;
+export * from './types/opencv';
+import './types/_cv';
